6dbafb6a0fdcf13b4a51768fe4a58bbcf72f9895
VerseTN <godoftheverse@gmail.com>
2024-09-16 21:18:48 +0100

    first commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,11 @@
+//INSTRUCTION 1
+public class ZooManagement {
+    // Main method
+    public static void main(String[] args) {
+        // Déclaration de variables dans ma main method, je n'ai qu'une seule classe
+        int nbrCages = 20;
+        String zooName = "my zoo";
+        // Affichage Message
+        System.out.println(zooName + " comporte " + nbrCages + " cages");
+    }
+}
